8009c3a0993717f357c6fceeb23a9ba1acdf8f6f
Paul Sweeney <paul-sweeney@outlook.ie>
2017-09-09 12:16:34 +0100

    Nix fixes

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var exec = require('child_process').exec;
 var cmd = path.resolve(process.cwd(), './node_modules/.bin/electron');
-var args = process.argv.slice(2).join(' ');
+var args = process.argv.slice(2).map(s => '"' + s + '"').join(' ');
 
 var child = exec(cmd + ' ' + __dirname + '/index.js ' + args);
 child.stdout.on('data', function(data) {
